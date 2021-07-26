a386e03571f8495d3f64731b2b6acd9c582f2f3e
demonicirfan <hashtag.irfan@gmail.com>
2021-07-26 21:39:35 +0530

    day 1 completed

---
+++ day1/first.py
@@ -0,0 +1,5 @@
+#Band name genearator
+print("welcome to the band name generator")
+name = input("what is your name ")
+pet_name = input("what is your pets name ")
+print("Your band name is --> " + name +" " + pet_name)
