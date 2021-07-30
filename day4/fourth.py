d1da0fd3a711ec4244debd2ccd58820f4a83e89f
irfan asif <hashtag.irfan@gmail.com>
2021-07-30 11:57:20 +0530

    day 4 first file

---
+++ day4/fourth.py
@@ -0,0 +1,8 @@
+#?list data structure
+states_of_america = ['Delware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts']
+
+print(states_of_america[0])
+print(states_of_america[-1])
+states_of_america[1]='nothing'
+
+
