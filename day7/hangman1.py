deaba9f53a8a5cbd24b143396a6bb73d8086621c
demonicirfan <hashtag.irfan@gmail.com>
2021-08-05 08:58:23 +0530

    python hangman

--- day7/hangman1.py
+++ day7/hangman1.py
@@ -1,20 +1,99 @@
 # step 1
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["ardvark", "baboon", "camel"]
 
 choosen_word = random.choice(word_list)
-choosen_word_len = len(choosen_word)
+choosen_word_length = len(choosen_word)
 
-guess = input("Guess a letter: ").lower()
+print('guessed word is ' + choosen_word)
 
-#letters = list(choosen_word)
+display = []
 
-array = []
+for _ in range(choosen_word_length):
+    display += "_"
+        
+i=1
+score = 0
+for i in range(choosen_word_length):
+    guess = input('Guess a letter: ').lower()
 
-for letter in choosen_word:
-    if letter == guess:
-        array.append(guess)
-    else:
-        array.append("_")
+    for position in range(choosen_word_length):
+        letter = choosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        else:
+            score = score + 1
+    if score == choosen_word_length:
+        print(stages[i])
+        i=i+1
+        
+    score = 0
+ #   life = choosen_word_length - i
+ #  print(f'punishment you have {life} life left')
+    print(display)
+    
 
-print(array)y
+
+l = list(choosen_word)
+if display == l:
+    print("\nYou won")
+
+else:
+    print("\nyou lost")
