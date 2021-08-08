2b7bbc2a228f0395a84f6d8b5a06164f5c9bf289
demonicirfan <hashtag.irfan@gmail.com>
2021-08-09 00:10:06 +0530

    day 8 completed

--- day8/caesar-cipher.py
+++ day8/caesar-cipher.py
@@ -1,16 +1,22 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+shift = shift % 26
 
 text_array = list(text)
 
+cipher_array = []
+
 
 def ceaser_cipher():
-    cipher_array = []
     for i in range(0, shift):
         index = alphabet.index(text_array[i])
         if direction == 'encode':
@@ -22,4 +28,14 @@ def ceaser_cipher():
     print(f"{' '.join(cipher_array)}")
 
 
-ceaser_cipher()
+xyz = []
+for char in text_array:
+    if char in alphabet:
+        xyz.append(1)
+    else:
+        xyz.remove(1)
+
+if len(xyz) == 5:
+    ceaser_cipher()
+else:
+    print("failed")
